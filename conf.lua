eab7137b5cfb6e2868275ce94f9456be607b0a10
Jean-André Santoni <jean.andre.santoni@gmail.com>
2012-06-27 19:34:46 +0200

    several improvements

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+  t.title = "Twin sisters"
+  t.author = "Jean-André Santoni, Lauriane Villaros, Océane Villaros"
+  t.url = "https://github.com/Kivutar/twinsisters"
+  t.version = "0.8.0"
+  t.screen.width = 800
+  t.screen.height = 600
+  t.screen.fullscreen = false
+end
