45fd6facaa94a9431a6442f70ab43c77bbf8222c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2012-08-06 13:07:03 +0200

    Add offset parameter for the generator

--- objects/generator.lua
+++ objects/generator.lua
@@ -1,17 +1,20 @@
 Generator = class('Generator')
 
-function Generator:initialize(w, x, y, z, type, period, max)
+function Generator:initialize(w, x, y, z, type, period, max, offset)
   self.w = w
   self.x = x
   self.y = y
   self.z = z
   self.type = type or 'Crab'
   self.period = period or 5
-  self.max = max or 3
+  self.max = max or -1
+  self.offset = offset or 1
 
-  CRON.every(self.period, function()
-  	if loadstring("return "..self.type..".instances < "..self.max)() then
-      addObject( { type=self.type, x=self.x, y=self.y, z=self.z }, self.w )
-	  end
+  CRON.after(self.offset, function()
+    CRON.every(self.period, function()
+      if self.max < 0 or loadstring("return "..self.type..".instances < "..self.max)() then
+        addObject( { type=self.type, x=self.x, y=self.y, z=self.z }, self.w )
+      end
+    end)
   end)
 end
