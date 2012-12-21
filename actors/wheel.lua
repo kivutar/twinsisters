77025615a442f32809bc815c356c45afebe37526
Jean-André Santoni <jean.andre.santoni@gmail.com>
2012-12-21 22:25:05 +0100

    Windmill wheel

---
+++ actors/wheel.lua
@@ -0,0 +1,19 @@
+Wheel = class('Wheel')
+
+Wheel.image = love.graphics.newImage('sprites/wheel.png')
+Wheel.image:setFilter("nearest", "nearest")
+
+function Wheel:initialize(x, y, z)
+  self.x = x
+  self.y = y
+  self.z = 10
+  self.r = 0
+end
+
+function Wheel:update(dt)
+  self.r = self.r - 0.5 * dt
+end
+
+function Wheel:draw()
+  love.graphics.draw(Wheel.image, self.x, self.y, self.r, 1, 1, 768/2, 768/2)
+end
--- main.lua
+++ main.lua
@@ -21,7 +21,7 @@ function love.load()
 
   love.graphics.setBackgroundColor(map.properties.r or 0, map.properties.g or 0, map.properties.b or 0)
 
-  camera:setScale(1.25)--(map.properties.zoom or 2))
+  camera:setScale(1.5)--(map.properties.zoom or 2))
 
   love.mouse.setVisible(false)
   
